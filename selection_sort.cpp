fa6f78e5369960fa176c25849dfcdd200e6309b0
Sathwik <sathwik677@gmail.com>
2025-10-07 13:47:43 +0530

    update

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -2,28 +2,29 @@
 using namespace std;
 
 int main(){
-    int arr[5] = {64,25,12,22,11};
+    int arr[5] = {56,23,67,12,68};
     int n = 5;
 
-    cout<<"Original array: ";
-    for(int i = 0; i < n; i++)
+    cout<<"Original array ";
+    for(int i=0;i<n;i++)
         cout<<arr[i]<<" ";
     cout<<endl;
-    
-    //selecton sort
-    for(int i=0; i<n-1; i++){
+
+    //selection sort
+    for(int i=0;i<n;i++){
         int mini = i;
-        for(int j=i+1; j<n; j++){
+        for(int j=i+1;j<n;j++){
             if(arr[j]<arr[i]){
                 mini = j;
             }
         }
         int temp = arr[i];
-        arr[i] = arr[mini];
+        arr[i]=arr[mini];
         arr[mini] = temp;
     }
-    cout<<"Sorted array: ";
-    for(int i=0; i<n; i++)
+    cout<<"Sorted array ";
+    for(int i=0;i<n;i++)
         cout<<arr[i]<<" ";
     cout<<endl;
+
 }
